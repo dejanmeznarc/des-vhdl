ae004c691af8755b91d22e1624093a7bdf980b1b
dejan <dejan@meznarc.si>
2024-04-27 19:38:43 +0200

    ok

--- sint24.vhd
+++ sint24.vhd
@@ -1,49 +1,44 @@
 -- Projekt: DES, sintetizator zvoka 2024
 
 library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
+  use IEEE.std_logic_1164.all;
+  use IEEE.numeric_std.all;
 
 entity sint24 is
- port (
-	clk  : in std_logic;  -- sistemska ura	
-	clk1 : out std_logic; -- serijska tipkovnica	
-	sdata: in std_logic;
-	shld : out std_logic;			
-	pwm1 : out std_logic; -- PWM
-	
-	data : inout unsigned(7 downto 0); -- IO modul
-	addr : out unsigned(1 downto 0); 
-	clkout: out std_logic;	
-	 
-	led  : out unsigned(7 downto 0); -- LED
+  port (
+    clk    : in    std_logic;            -- sistemska ura	
+    clk1   : out   std_logic;            -- serijska tipkovnica	
+    sdata  : in    std_logic;
+    shld   : out   std_logic;
+    pwm1   : out   std_logic;            -- PWM
 
-   sw  : in unsigned(3 downto 0); -- butns
-	key  : in unsigned(1 downto 0) -- butns
- );
-end sint24;
+    data   : inout unsigned(7 downto 0); -- IO modul
+    addr   : out   unsigned(1 downto 0);
+    clkout : out   std_logic;
 
-architecture RTL of sint24 is 
+    led    : out   unsigned(7 downto 0); -- LED
 
-signal counter:  unsigned(30 downto 0);
+    sw     : in    unsigned(3 downto 0); -- butns
+    key    : in    unsigned(1 downto 0)  -- butns
+  );
+end entity;
+
+architecture RTL of sint24 is
+
+  signal clk1hz  : std_logic;
+  signal clk10hz : std_logic;
 
 begin
   led(0) <= key(0);
-    
-  
-  process(clk)
-  begin
-  
-  if(rising_edge(clk)) then
-	counter <= counter + 1;
-  end if;
- 
-  end process;
-  
-  
-  
-  led(1) <= counter(26);
-  
-  
-  
-end RTL;
+
+  u1: entity work.clocks
+    port map (
+      clkin   => clk,
+      clk1hz  => clk1hz,
+      clk10hz => clk10hz
+    );
+
+  led(1) <= clk1hz;
+  led(2) <= clk10hz;
+
+end architecture;
