7795971d2bc18420b7ebf7bbb39a678461fcc62f
dejan <dejan@meznarc.si>
2024-04-30 12:44:50 +0200

    cleande dup

--- sint24.vhd
+++ sint24.vhd
@@ -14,50 +14,18 @@ end entity;
 
 architecture RTL of sint24 is
 
-  signal slowclk        : std_logic;
-  signal ultraSlowClock : std_logic;
-  signal counter        : unsigned(22 downto 0);
-
-  signal address : unsigned(1 downto 0);
-
-  signal matrixData : std_logic_vector(7 downto 0);
-  signal matrixLine : unsigned(2 downto 0);
-  signal matrixCols : unsigned(4 downto 0);
-
+  signal matrixData : unsigned(7 downto 0);
   signal buttonData : unsigned(3 downto 0);
 
 begin
-
-  clkdownsizer: process (clk)
-  begin
-    if (rising_edge(clk)) then
-      counter <= counter + 1;
-    end if;
-  end process; -- clkdownsizer
-  slowclk        <= counter(10);
-  ultraSlowClock <= counter(22);
-  pin_led(0)     <= slowclk;
-  pin_led(1)     <= ultraSlowClock;
-
-  pin_led(7 downto 4) <= unsigned(buttonData);
-  matrixData          <= std_logic_vector(matrixLine & matrixCols);
-
   interface_inst: entity work.interface
     port map (
       clk      => clk,
       buttons  => buttonData,
-      matrix   => unsigned(matrixData),
+      matrix   => matrixData,
       pin_addr => pin_io_addr,
       pin_data => pin_io_data,
       pin_clk  => pin_io_clkout
     );
-
-  cddd: process (ultraSlowClock)
-  begin
-    if rising_edge(ultraSlowClock) then
-      matrixLine <= matrixLine + 1;
-    end if;
-    matrixCols <= "11011";
-  end process; -- cddd
 
 end architecture;
