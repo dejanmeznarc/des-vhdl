cad61049d942556ea25d0d459eeface32ce21607
dejan <dejan@meznarc.si>
2024-05-01 20:18:08 +0200

    figs in sepparate file

---
+++ src/common/figures.vhd
@@ -0,0 +1,28 @@
+library IEEE;
+  use IEEE.std_logic_1164.all;
+  use IEEE.numeric_std.all;
+  use work.screen_pkg.all;
+  use work.figure_type.all;
+
+package figures is
+
+  constant figuresRom : figures_t := (
+    (
+      "101",
+      "010",
+      "101"
+    ),
+    (
+      "110",
+      "010",
+      "000"
+    ),
+    (
+      "000",
+      "000",
+      "111"
+    )
+  );
+
+
+end package;
--- src/graphics/gpu_figure_drawer.vhd
+++ src/graphics/gpu_figure_drawer.vhd
@@ -2,10 +2,12 @@ library IEEE;
   use IEEE.std_logic_1164.all;
   use IEEE.numeric_std.all;
   use work.screen_pkg.all;
+  use work.figure_type.all;
+  use work.figures.all;
 
 entity gpu_firuge_drawer is
   port (
-    figureID : in  unsigned(2 downto 0); -- wha fig to display
+    figureID : in  unsigned(2 downto 0);                    -- wha fig to display
 
     cord_x   : in  unsigned(2 downto 0) := (others => '0'); -- col
     cord_y   : in  unsigned(2 downto 0) := (others => '0'); -- line
@@ -15,32 +17,12 @@ entity gpu_firuge_drawer is
 end entity;
 
 architecture rtl of gpu_firuge_drawer is
-  type figure_t is array (0 to 2) of unsigned(0 to 2);
-  type figures_t is array (0 to 2) of figure_t;
-  constant figs : figures_t := (
-    (
-      "101",
-      "010",
-      "101"
-    ),
-    (
-      "110",
-      "010",
-      "000"
-    ),
-    (
-      "000",
-      "000",
-      "111"
-    )
-  );
-
   signal figure : figure_t;
 
   signal janez : unsigned(4 downto 0);
 begin
 
-  figure <= figs(to_integer(figureID));
+  figure <= figuresRom(to_integer(figureID));
 
   draw: process (figure, cord_x, cord_y)
   begin
@@ -48,11 +30,9 @@ begin
     screen <= (others => (others => '0'));
 
     -- janez <= (4 downto (to_integer(cord_x) + 1) => '0') & "111" & ((to_integer(cord_x) - 1) downto 0 => '0');
-
     screen(to_integer(cord_y - 1)) <= (("00000" & figure(0)) sll to_integer(cord_x)) srl 1;
     screen(to_integer(cord_y - 0)) <= (("00000" & figure(1)) sll to_integer(cord_x)) srl 1;
     screen(to_integer(cord_y + 1)) <= (("00000" & figure(2)) sll to_integer(cord_x)) srl 1;
-
 
   end process; -- identifier
 
